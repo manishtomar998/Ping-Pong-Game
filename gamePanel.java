2c60b8931aaf4c721a960bac35790fcf28573555
unknown <manishtomar998@gmail.com>
2022-09-13 12:57:20 +0530

    Ball Movement

--- gamePanel.java
+++ gamePanel.java
@@ -32,7 +32,9 @@ public class gamePanel extends JPanel implements Runnable {
     }
 
     public void newBall() {
-
+        ball = new gameBall((GAME_WIDTH / 2) - (BALL_DIAMETER / 2), (GAME_HEIGHT / 2) - (BALL_DIAMETER / 2),
+                BALL_DIAMETER,
+                BALL_DIAMETER);
     }
 
     public void newPaddles() {
@@ -51,14 +53,45 @@ public class gamePanel extends JPanel implements Runnable {
     public void draw(Graphics g) {
         paddle1.draw(g);
         paddle2.draw(g);
+        ball.draw(g);
     }
 
     public void move() {
         paddle1.move();
         paddle2.move();
+        ball.move();
     }
 
     public void checkCollision() {
+        // bounces ball off top and bottom window edges
+        if (ball.y <= 0) {
+            ball.setYDirection(-ball.yVelocity);
+        }
+        if (ball.y >= GAME_HEIGHT - BALL_DIAMETER) {
+            ball.setYDirection(-ball.yVelocity);
+        }
+        // bounces ball off paddles
+        if (ball.intersects(paddle1)) {
+            ball.xVelocity = Math.abs(ball.xVelocity);
+            ball.xVelocity++;
+            if (ball.yVelocity > 0)
+                ball.yVelocity++;
+            else
+                ball.yVelocity--;
+            ball.setXDirection(ball.xVelocity);
+            ball.setYDirection(ball.yVelocity);
+        }
+        if (ball.intersects(paddle2)) {
+            ball.xVelocity = Math.abs(ball.xVelocity);
+            ball.xVelocity++;
+            if (ball.yVelocity > 0)
+                ball.yVelocity++;
+            else
+                ball.yVelocity--;
+            ball.setXDirection(-ball.xVelocity);
+            ball.setYDirection(ball.yVelocity);
+        }
+
         if (paddle1.y <= 0) {
             paddle1.y = 0;
         }
